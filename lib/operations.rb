1704b6ac820ed565d20f061fb24e4d198324d734
Gilberto Ocampo <ocampo@gate.net>
2020-02-14 23:25:16 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,15 @@
 require 'pry'
 
-binding pry
-
 def unsafe?(speed)
-	if speed < 40 
+  binding.pry
+
+if speed < 40 
 	  true
-	elsif speed > 60
+elsif speed > 60
 	  true
-	else speed >== 40 and <==60
+else speed >== 40 and <==60
 	  false
 end
+
 end
 
