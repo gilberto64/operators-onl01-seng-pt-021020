89f21591e82d8ad3a236407672b41a8c47e73234
Gilberto Ocampo <ocampo@gate.net>
2020-02-15 01:59:50 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -17,11 +17,12 @@ end
 
 def not_safe?(speed)
  
- speed >= 40 && <= 60 ? 'true' : 'false'
-  
- ## if speed >= 40 ? true : false
+ (speed >= 40 && <= 60) ? true : false
 
+end
+
+  
+## if speed >= 40 ? true : false
 ## else
 ##   speed > 60 ? false : true
 
-end
